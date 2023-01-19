e33c67aeee52efed59312157141fd1f77a066131
nawan90 <rachmat.d.gunawan@gmail.com>
2023-01-19 15:04:55 +0700

    update vercel

--- src/util/Api.js
+++ src/util/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const httpClient = axios.create({
   // baseURL: `https://usermgmt.raspi-geek.com/`,
-  baseURL: `http://g-axon.work/jwtauth/api/`, //YOUR_API_URL HERE
+  baseURL: `https://g-axon.work/jwtauth/api/`, //YOUR_API_URL HERE
 
   headers: {
     "Content-Type": "application/json",
